689cc9f0e39a64ee578c5988040313821b27e662
Suru++ Project <suru-plus@users.noreply.github.com>
2020-08-12 11:34:32 -0300

    Backing up and updating Hugo site

--- modify-date.sh
+++ modify-date.sh
@@ -1,6 +1,13 @@
 git filter-branch --env-filter \
-    'if [ $GIT_COMMIT = 0ff36d1a43b79da76742c2675996352b2043b4b8 ]
+    'if [ $GIT_COMMIT = d3871686b9a46105737068b92062e9b3dd81eaa3 ]
      then
-         export GIT_AUTHOR_DATE="Fri Aug 12 11:08:30 2020 -0300"
-         export GIT_COMMITTER_DATE="Sat Aug 12 11:08:30 2020 -0300"
+         export GIT_AUTHOR_DATE="Wed Aug 12 10:45:30 2020 -0300"
+         export GIT_COMMITTER_DATE="Wed Aug 12 10:45:30 2020 -0300"
+     fi'
+
+git filter-branch --env-filter \
+    'if [ $GIT_COMMIT = d0bf7a9182d28161889f9b061d0e5975b7d0a992 ]
+     then
+         export GIT_AUTHOR_DATE="Wed Aug 12 10:57:32 2020 -0300"
+         export GIT_COMMITTER_DATE="Wed Aug 12 10:57:32 2020 -0300"
      fi'
