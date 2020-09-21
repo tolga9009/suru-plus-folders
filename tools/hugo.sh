8ee065683c1f93fbb25adae0f40ada762aef0d9c
Suru++ Project <suru-plus@users.noreply.github.com>
2020-09-21 05:08:00 -0300

    Added a script to run automatically

---
+++ tools/hugo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PID=`ps -eaf | grep hugo | grep -v grep | awk '{print $2}'`
+
+if [[ "" !=  "$PID" ]]; then
+  echo "Killing Hugo"
+  killall -9 hugo
+fi
+
+hugo server --config 'config.yaml' -l 'layouts' -b localhost -p 1989 --gc
+
+# hugo server --config 'config.yaml' -l 'layouts' -b glocalhost -p 1989 -F --gc
+# hugo server --config 'config.yaml' -l 'layouts' -F --gc --renderToDisk
