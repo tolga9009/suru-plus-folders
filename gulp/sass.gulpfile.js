e7a0b26976306455ec40f4074887f2ab7e9e2cf6
Suru++ Project <suru-plus@users.noreply.github.com>
2020-12-27 23:31:37 -0300

    Configuração de Gulp para SASS atualizada

--- gulp/sass.gulpfile.js
+++ gulp/sass.gulpfile.js
@@ -1,16 +1,23 @@
 'use strict';
  
+var csscomb = require('gulp-csscomb');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
  
 sass.compiler = require('node-sass');
  
-gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+gulp.task('sass', function () 
+{
+  return gulp.src('./../static/assets/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./../static/assets/css/'));
 });
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+
+gulp.task('csscomb', function() 
+{
+  return gulp.src('./../static/assets/css/schemes.css')
+    .pipe(csscomb())
+    .pipe(gulp.dest('./../static/assets/css/'));
 });
+
+gulp.task('sass-csscomb', gulp.parallel('sass', 'csscomb')); 
