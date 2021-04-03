34b179c267c4270276584ce16552c612337c0bbc
Suru++ Project <suru-plus@users.noreply.github.com>
2021-04-03 13:36:32 -0300

    Removed unncessary folders

--- tools/copy.sh
+++
@@ -1,6 +0,0 @@
-CAMINHO="$HOME/GitHub/adwaita-plus-private/Adwaita++/places/scalable"
-DESTINO="$HOME/GitHub/suru-plus-folders/images/adwaita-plus" 
-
-cp $CAMINHO/folder-{adwaita,aubergine,black,blue,bluegrey,bordeaux,brown,canonical,custom,green,grey,indigo,manjaro,mint,orange,purple,red,white,yaru,yellow}-{app,documents,downloads,dropbox,gitlab,music,open,pictures,visiting}.svg $DESTINO
-
-cp $CAMINHO/folder-{adwaita,aubergine,black,blue,bluegrey,bordeaux,brown,canonical,custom,green,grey,indigo,manjaro,mint,orange,purple,red,white,yaru,yellow}.svg $DESTINO
